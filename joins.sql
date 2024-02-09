dcfe6b3a7a012187ab4aca6d937de344e521ac89
Jack Usery <83471718+JackUsery@users.noreply.github.com>
2024-02-09 13:45:56 -0600

    Inner joins

--- joins.sql
+++ joins.sql
@@ -10,8 +10,43 @@
 # INNER JOIN user_skills
 # ON users.user_id = user_skills.user_id
 
+SELECT *
+FROM customers;
+
+SELECT *
+FROM customer_orders;
+
+SELECT *
+FROM customers c
+INNER JOIN customer_orders co
+	ON c.customer_id = co.customer_id
+ORDER BY c.customer_id;
 
 
+SELECT *
+FROM products;
+
+SELECT *
+FROM customer_orders;
+
+SELECT product_name, SUM(order_total) AS Total
+FROM products p
+JOIN customer_orders co
+	ON p.product_id = co.product_id
+GROUP BY product_name
+ORDER BY 2;
+
+
+SELECT *
+FROM suppliers;
+
+SELECT *
+FROM ordered_items;
+
+SELECT *
+FROM suppliers s
+INNER JOIN ordered_items oi
+	ON s.supplier_id = oi.shipper_id;
 
 
 
