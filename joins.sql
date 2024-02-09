324b6ebe98d9d2de0eee64a62635044ea1e4ea20
Jack Usery <83471718+JackUsery@users.noreply.github.com>
2024-02-09 13:24:37 -0600

    Joins intro

--- joins.sql
+++ joins.sql
@@ -1,14 +1,36 @@
 
-# inner join
+
+
+
+
+# inner join - returns records that have matching values in both tables
+
+# SELECT *
+# FROM users
+# INNER JOIN user_skills
+# ON users.user_id = user_skills.user_id
+
+
+
+
+
+
 
 # outer joins
 
+# Right join - returns all records from the right table, and matched records from the left table
 
-# self joins
+# Left join - returns all records from the left table, and matched records from the right table
+
+# Full join - returns all records from both tables
+
+# self joins - table is joined to itself: aliases are required
 
 
-# cross joins
+# cross joins - returns all possible combinations of all rows: no 'ON' clause
 
 
-#
+# Unions - combine the results of two or more SELECT statements into a single result
+
+
 
