78e019dea12bc0d3c39db3ae354ab114669e9a9a
JackUsery <Jack.usery1@gmail.com>
2024-03-13 15:31:20 -0500

    Update joins.sql
    Joining on Multiple Conditions

--- joins.sql
+++ joins.sql
@@ -72,6 +72,31 @@ JOIN customer_orders co
 JOIN customers c
 	ON co.customer_id = c.customer_id;
     
+# Joining on Multiple Conditions
+
+SELECT *
+FROM customer_orders;
+
+SELECT *
+FROM customer_orders_review;
+
+SELECT *
+FROM customer_orders co
+JOIN customer_orders_review cor
+	ON co.order_id = cor.order_id;
+
+SELECT *
+FROM customer_orders co
+JOIN customer_orders_review cor
+	ON co.order_id = cor.order_id
+	AND co.customer_id = cor.customer_id;
+    
+SELECT *
+FROM customer_orders co
+JOIN customer_orders_review cor
+	ON co.order_id = cor.order_id
+	AND co.customer_id = cor.customer_id
+    AND co.order_date = cor.order_date;
     
 # outer joins
 
