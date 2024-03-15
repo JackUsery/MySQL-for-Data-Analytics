471dde45cb9aed1a92d962b845ec260389fba5f1
JackUsery <Jack.usery1@gmail.com>
2024-03-15 15:13:10 -0500

    added files
    added regular expression file and started subqueries

---
+++ subqueries.sql
@@ -0,0 +1,62 @@
+# Subquery Introduction
+
+# A Subquery is essentially a query within a query.
+
+#Example:
+#SELECT customer_name, customer_id
+#FROM customers
+#WHERE customer_id 
+#IN (SELECT customer_id 
+       #FROM orders 
+       #WHERE order_amount > 1000);
+
+
+
+
+
+# Subquery Basics
+
+
+
+
+
+
+
+
+
+# ANY and ALL Operators
+
+
+
+
+
+
+
+# Exists
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Subqueries in Select and From
+
+
+
+
+
+
+
+
